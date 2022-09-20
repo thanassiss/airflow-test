2cc3fc27dd2958e94537486a690b7ab790744500
thanassiss <athanasios.spyriou@arabesque.com>
2022-09-20 16:19:28 +0100

    update

--- dags/airflow_bash_operator.py
+++ dags/airflow_bash_operator.py
@@ -1,76 +1,40 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""Example DAG demonstrating the usage of the BashOperator."""
-from __future__ import annotations
-
-import datetime
-
-import pendulum
-
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
 
-with DAG(
-    dag_id='example_bash_operator',
-    schedule='0 0 * * *',
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    catchup=False,
-    dagrun_timeout=datetime.timedelta(minutes=60),
-    tags=['example', 'example2'],
-    params={"example_key": "example_value"},
-) as dag:
-    run_this_last = EmptyOperator(
-        task_id='run_this_last',
-    )
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 12, 2),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-    # [START howto_operator_bash]
-    run_this = BashOperator(
-        task_id='run_after_loop',
-        bash_command='echo 1',
-    )
-    # [END howto_operator_bash]
+def sample_python_function(**kwargs):
+    """
+    Insert arbitrary python function here
 
-    run_this >> run_this_last
+    """
 
-    for i in range(3):
-        task = BashOperator(
-            task_id='runme_' + str(i),
-            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        )
-        task >> run_this
+dag = DAG('example_python_operator',
+            max_active_runs=3,
+            catchup=True,
+            schedule_interval='@daily',
+            default_args=default_args)
 
-    # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id='also_run_this',
-        bash_command='echo "ti_key={{ task_instance_key_str }}"',
-    )
-    # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
+with dag:
 
-# [START howto_operator_bash_skip]
-this_will_skip = BashOperator(
-    task_id='this_will_skip',
-    bash_command='echo "hello world"; exit 99;',
-    dag=dag,
-)
-# [END howto_operator_bash_skip]
-this_will_skip >> run_this_last
+    start = DummyOperator(task_id='start')
 
-if __name__ == "__main__":
-    dag.cli()
+    for i in range(0,10):
+
+        t1 = PythonOperator( 
+            task_id='python_function_viraj_{0}'.format(i),
+            python_callable=sample_python_function,
+            dag=dag)
+
+        start >> t1
