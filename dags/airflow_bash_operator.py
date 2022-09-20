c311e4a46fda61b81493ca4d1b9a8543a9b642b4
thanassiss <athanasios.spyriou@arabesque.com>
2022-09-20 16:33:52 +0100

    update

--- dags/airflow_bash_operator.py
+++ dags/airflow_bash_operator.py
@@ -1,40 +1,26 @@
+"""A liveness prober dag for monitoring composer.googleapis.com/environment/healthy."""
+import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2019, 12, 2),
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': airflow.utils.dates.days_ago(0),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=5)
 }
 
-def sample_python_function(**kwargs):
-    """
-    Insert arbitrary python function here
+dag = DAG(
+    'airflow_monitoring',
+    default_args=default_args,
+    description='liveness monitoring dag',
+    schedule_interval=None,
+    dagrun_timeout=timedelta(minutes=20))
 
-    """
-
-dag = DAG('example_python_operator',
-            max_active_runs=3,
-            catchup=True,
-            schedule_interval='@daily',
-            default_args=default_args)
-
-with dag:
-
-    start = DummyOperator(task_id='start')
-
-    for i in range(0,10):
-
-        t1 = PythonOperator( 
-            task_id='python_function_viraj_{0}'.format(i),
-            python_callable=sample_python_function,
-            dag=dag)
-
-        start >> t1
+# priority_weight has type int in Airflow DB, uses the maximum.
+t1 = BashOperator(
+    task_id='echo',
+    bash_command='echo test',
+    dag=dag,
+    depends_on_past=False,
+    priority_weight=2**31-1)
